a571c14cacb0549b8e3ec1cd35583266587ef0a9
pocopota <pocopota.program@gmail.com>
2025-05-30 01:32:34 +0900

    リアルタイムデータ取得

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+// 公開してOKなFirebase構成
+export const firebaseConfig = {
+  apiKey: "AIzaSyBsDouR08nSVm1Z_YMplmJhSfWhD9KRdDY",
+  authDomain: "yado-pos.firebaseapp.com",
+  projectId: "yado-pos",
+  storageBucket: "yado-pos.firebasestorage.app",
+  messagingSenderId: "948509330895",
+  appId: "1:948509330895:web:3e6d782bd8052b35f416ba"
+};
